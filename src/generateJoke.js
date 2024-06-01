aa4ae383669969c6c37e09b57ab0182ac7085788
Ahmet Güleş <64443271+CagKebabi@users.noreply.github.com>
2024-06-01 17:03:54 +0300

    update

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,5 @@
 function generateJoke(params) {
-  return "I don't trust stairs. They're always up to something";
+  return "I don't trust stairs. They're always up to something!";
 }
 
 export default generateJoke;
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"), //derlenen ve yeni oluşturulan dosyanın hangi dizinde oluşturulacağını belirtir
     filename: "[name][contenthash].js", //derlenen ve yeni oluşturulan dosyanın ismini entry kısmındaki bundle ismiyle oluşturur oraya ne yazarsak o isimle oluşturacaktır.
+    clean: true, //filename e [contenthash] eklediğimiz için her build aldığımızda js dosyamızın ismi değişiyordu ve önceki buildlerde oluşan dosyalar distin içerisinde kalıyordu bu mout ile build aldığımızda önceki dosyanın veya dosyaların otomatik silinmesini sağlamış olduk.
+  },
+  devtool: "source-map", //her build aldığımızda js dosyamız için .map (source-map) uzantılı dosya oluşturur
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"), //projedeki hangi dosyayı devserverde çalıştıracağımızı belirtir.
+    },
+    port: 3000, //dev serverın çalışacağı portu belirtir varsayılan (8080) dir
+    //open: true, //npm run dev i çalıştırdığımızda otomatik olarak tarayıcıyı açmasını belirtir.(Bunu açınca dev serverı çalıştıramıyor ve hata alıyorum)
+    hot: true, //hotreload ın açık veya kapalı olmasını belirtir.
+    compress: true, //gzip sıkıştırmasını etkinleştirir
+    historyApiFallback: true, //https://www.quora.com/What-is-History-API-Fallback
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -25,7 +37,7 @@ module.exports = {
       },
     ],
   },
-  //   watch: true,
+  //watch: true,
 };
 
 //HTMLPlugin src de oluşturduğumuz html sayfamızı derlememizi sağlar. dist klasörünü silsek dahi tekrar build ettğimizde html ve js
@@ -49,3 +61,17 @@ module.exports = {
 //loaderları yönetebilmek için webpack.config.js de modeule: {} objesi oluşturuyoruz ve içerisinde hangi uzantılı dosyalarda hangi loaderları
 //kullanacağımızı belirtiyoruz.Artık build aldığımızda loaderlar sayesinde sass otomatik compile edilecek ve webpackimize eklenecek.
 //İstersek farklı scss dosyaları oluşturup main.scss dosyası içerisinde import edebiliriz.
+
+//Webpack dev server development modunda her değişikliklikte build almadan değişikliği görmemizi sağlar.
+//kurulumu için önce package.json script kısmına "dev": "webpack serve" scriptini ekliyoruz.  terminale npm run dev yazıyoruz
+//terminalde wepback webpack-dev-server kurulumu için onay istiyor y ye basıp kurulumu yapıyoruz istersek
+//manuel olarak npm i -D webpack-dev-server komutu ile de yapabliriz.Bu adımları tamamladıktan sonra
+//npm run dev komutunu çalıştırırsak projemiz varsayılan olarak http://localhost:8080/ portunda açılacaktır.
+//webpack serveri özelleştirmek için webpack.config dosyamızda değişiklikler yapabiliriz. devServer{}
+//dist dosyamızı silip dev serverı çalıştırırsak dosyamızı silmemize rağmen sayfamızın açıldığını görebiliriz
+//bunun sebebi dosyamızı diskten değil bellekten çalıştırıyor olmasıdır.
+
+//source map ler hata ayıklamak için iyidir çünkü çoğu zaman bize asıl sorunun nerede olduğunu göstermeyen satır numarasına sahip hata mesajları
+//alırız. dist, production kodumuz için veya kaynak kodumuz için bize bir harita .map oluşturur.
+//Oluşturmak için webpack.config dosyamıza devTool: 'source-map', eklememiz yeterlidir.Böylece her build aldığımızda js dosyamızın
+//.map uzantılı source-map i de oluşturulacaktır.
